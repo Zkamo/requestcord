f639dba0af7a8933c2d587732654d660a8d8bed7
Kamo <163022802+Zkamo@users.noreply.github.com>
2025-03-15 15:06:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 from setuptools import setup, find_packages
 
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
 setup(
     name="requestcord",
-    version="1.0.0b0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "requests",
         "curl-cffi",
         "websocket-client",
+        "websockets",
         "colorama",
-        "discord-protos"
+        "discord-protos",
+        "aiohttp",
+        "asyncio"
     ],
     extras_require={
         "win": ["pywin32>=306; sys_platform == 'win32'"]
     },
     description="Advanced Discord API wrapper with modern features",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     author="Kamo",
     license="MIT",
     classifiers=[
@@ -22,6 +31,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
-    license_files=[],
+    python_requires=">=3.8"
 )
