4c2cb95d1aceb885d16a5a9c5ed2233cf86066a3
Kamo <163022802+Zkamo@users.noreply.github.com>
2025-03-15 15:04:20 +0100

    Update __init__.py

--- requestcord/__init__.py
+++ requestcord/__init__.py
@@ -1,27 +1,14 @@
-import re
-import json
-import time
-import base64
-import random
-import platform
-import requests
-import websocket
-import threading
+from typing import NewType
 
-from typing import Tuple, Dict, List, Any, Optional
-from datetime import datetime
-from json import dumps, loads
-from curl_cffi import requests
-from websocket import WebSocket
-from colorama import Fore, Style
-from urllib.parse import urlencode
-from curl_cffi.requests import RequestsError
+Snowflake                         = NewType('Snowflake', int)
 
-from requestcord.Logger import Logger
-from discord_protos import PreloadedUserSettings
-from requestcord.DiscordFetch import Build
-from requestcord.DiscordFetch import Session
-from requestcord.DiscordHeaders import HeaderGenerator
-from requestcord.DiscordBypass import Bypass
-from requestcord.DiscordChange import ServerEditor
-from requestcord.DiscordChange import ProfileEditor
+from requestcord.Logger           import Logger
+from discord_protos               import PreloadedUserSettings
+from requestcord.DiscordFetch     import Build
+from requestcord.DiscordFetch     import SessionID
+from requestcord.DiscordGateway   import Presence
+from requestcord.DiscordHeaders   import HeaderGenerator
+from requestcord.DiscordBypass    import Bypass
+from requestcord.DiscordChange    import ServerEditor
+from requestcord.DiscordChange    import ProfileEditor
+from requestcord.DiscordRequests  import APIRequests
